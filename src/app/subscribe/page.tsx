9bb30ebfd8fca3af7edf78e3ae3f917d41e94c4c
avrxt Cloud <fliqva@gmail.com>
2026-02-07 16:30:00 +0530

    Update page.tsx

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -18,7 +18,7 @@ export default function Subscribe() {
         const data = Object.fromEntries(formData);
 
         try {
-            const response = await fetch('https://api.avrxt.in/api/subscribe', {
+            const response = await fetch('/api/subscribe', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data),
