824d9f5ff186e19476340b4edbec977844d185e6
avrxt Cloud <fliqva@gmail.com>
2026-03-24 18:26:34 +0530

    Refactor robots.txt rules for user agents

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,19 +2,25 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: [
-                '/me/admin',
-                '/docs/admin',
-                '/api/',
-                '/auth/',
-                '/login',
-                '/_next/',
-                '/private/',
-            ],
-        },
+        rules: [
+            {
+                userAgent: ['GPTBot', 'ClaudeBot', 'anthropic-ai', 'Bytespider', 'CCBot'],
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: ['/', '/_next/static/'],
+                disallow: [
+                    '/me/admin',
+                    '/docs/admin',
+                    '/api/',
+                    '/auth/',
+                    '/login',
+                    '/_next/',
+                    '/private/',
+                ],
+            },
+        ],
         sitemap: 'https://www.avrxt.in/sitemap.xml',
     };
 }
